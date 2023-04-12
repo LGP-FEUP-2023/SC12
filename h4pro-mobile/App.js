bb81dac219e0550258ce39c42164a8ae904d369c
sergio estevao <smestevao11@gmail.com>
2023-04-12 15:05:23 +0100

    feat: added save view

--- h4pro-mobile/App.js
+++ h4pro-mobile/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
