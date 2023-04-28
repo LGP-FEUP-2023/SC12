973a8fb8eeaae02965f2d50e3a0ab4554390a78d
sergio estevao <smestevao11@gmail.com>
2023-04-28 15:48:48 +0100

    feat: api_utils added

--- h4pro-mobile/App.js
+++ h4pro-mobile/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MainPage from './src/views/main-page.js'
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SessionScanner from './src/views/session-scanner.js';
+import { Linking } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 Stack.Navigator.defaultProps = {
@@ -12,7 +12,43 @@ Stack.Navigator.defaultProps = {
   },
 };
 
+const handleDeepLink = (event) => {
+  const url = event.url;
+  const { path, queryParams } = Linking.parse(url);
+
+  const { padel_company_id, padel_court_id } = queryParams;
+  const joinCourtUrl = `https://MEM4PRO/ID_PLACEHOLDER/join_court/${padel_company_id}/${padel_court_id}`;
+
+  fetch(joinCourtUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log("[SUCCESS]:", data)
+  })
+  .catch(error => {
+    console.log("[ERROR]:", error)
+  });
+};
+
 const App = () => {
+
+  useEffect(() => {
+    const handleInitialUrl = async (url) => {
+      if (url) {
+        handleDeepLink({ url });
+      }
+    };
+
+    Linking.getInitialURL().then(handleInitialUrl);
+    Linking.addEventListener('url', handleDeepLink);
+
+    return () => Linking.removeEventListener('url', handleDeepLink);
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -23,4 +59,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
