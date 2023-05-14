0decfcb47fb14938b522981ba96f2d8d49b3373d
eukia <ritam.mendes@gmail.com>
2023-05-14 16:37:14 +0100

    refactor: use react hook forms

--- h4pro-mobile/src/views/login-page.js
+++ h4pro-mobile/src/views/login-page.js
@@ -1,66 +1,91 @@
-import styles from '../styles/login-page.style'
-import { IMAGES } from '../constants/images'
-import { MyStatusBar } from '../components/status-bar'
-import FormTextInput from '../components/form-text-input';
-import React, { Component } from 'react'
-import { View, Image, Pressable } from 'react-native'
-import { Text } from 'react-native'
 
-class LoginPage extends Component {
+import React from 'react';
+import { View, Image, TextInput, Pressable } from 'react-native';
+import { Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { IMAGES } from '../constants/images';
+import { MyStatusBar } from '../components/status-bar';
+import styles from '../styles/login-page.style';
 
-  handleSubmit() {
-    console.log("Submitting form")
-  }
+import { COLOR } from '../constants/colors';
+export default function LoginPage() {
+  const { control, formState: { errors }, register, handleSubmit, getValues } = useForm();
 
-  redirectSignUp() {
-    console.log("Signup")
-  }
+  const onSubmit = (data) => {
+    console.log('Submitting form', data);
+  };
 
-  redirectForgotPassword() {
-    console.log("Forgot")
-  }
+  const redirectSignUp = () => {
+    console.log('Signup');
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MyStatusBar />
-        <View
-          style={[
-            styles.container,
-            {
-              flexDirection: 'column',
-            },
-          ]}>
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Image style={styles.logoBig}
-              source={IMAGES.logo}
+  const redirectForgotPassword = () => {
+    console.log('Forgot');
+  };
+
+  return (
+    <View style={styles.container}>
+      <MyStatusBar />
+      <View style={[styles.container, { flexDirection: 'column' }]}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image style={styles.logoBig} source={IMAGES.logo} />
+        </View>
+        <View style={{ flex: 1, alignItems: 'center', gap: 22 }}>
+          <View style={styles.formTextContainer}>
+            <Image source={IMAGES.user} />
+            <Controller
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={styles.formTextInput}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholder="EMAIL"
+                  placeholderTextColor={COLOR.lightGray}
+                />
+              )}
+              name="email"
+              defaultValue=""
             />
+            {errors.email && <Text>This field is required.</Text>}
           </View>
-          <View style={{ flex: 1, alignItems: 'center', gap: 22 }}>
-            <View style={styles.formTextContainer}>
-              <Image source={IMAGES.user} />
-              <FormTextInput placeholder="USERNAME OR EMAIL" />
-            </View>
-            <View style={styles.formTextContainer}>
-              <Image source={IMAGES.password} />
-              <FormTextInput placeholder="PASSWORD" hide={true} />
-            </View>
-            <Pressable onPress={this.redirectForgotPassword}>
-              <Text style={styles.linkBlue}>FORGOT YOUR PASSWORD?</Text>
-            </Pressable>
+          <View style={styles.formTextContainer}>
+            <Image source={IMAGES.password} />
+            <Controller
+              control={control}
+              rules={{ required: true }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={styles.formTextInput}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  placeholder="PASSWORD"
+                  placeholderTextColor={COLOR.lightGray}
+                  secureTextEntry={true}
+                />
+              )}
+              name="password"
+              defaultValue=""
+            />
+            {errors.password && <Text>This field is required.</Text>}
           </View>
-          <View style={{ flex: 1, alignItems: 'center', gap: 17 }}>
-            <Pressable style={styles.button} onPress={this.handleSubmit} >
-              <Text style={styles.buttonText}>SIGN IN</Text>
-            </Pressable>
-            <Pressable onPress={this.redirectSignUp}>
-              <Text style={styles.linkWhite}>OR SIGN UP INSTEAD</Text>
-            </Pressable>
-          </View>
+          <Pressable onPress={redirectForgotPassword}>
+            <Text style={styles.linkBlue}>FORGOT YOUR PASSWORD?</Text>
+          </Pressable>
+        </View>
+        <View style={{ flex: 1, alignItems: 'center', gap: 17 }}>
+          <Pressable style={styles.button} onPress={handleSubmit(onSubmit)}>
+            <Text style={styles.buttonText}>SIGN IN</Text>
+          </Pressable>
+          <Pressable onPress={redirectSignUp}>
+            <Text style={styles.linkWhite}>OR SIGN UP INSTEAD</Text>
+          </Pressable>
         </View>
       </View>
-    )
-  }
+    </View>
+  );
 }
 
-export default LoginPage
