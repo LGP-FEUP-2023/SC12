c36aed8d4901fc3c06e8e61fd5a8badb7d5c5fa0
up201904925 <up201904925@fe.up.pt>
2023-05-15 18:28:02 +0100

    cleaned code

--- PADEL4PROAndroid/wear/src/main/java/feup/edu/lgp/padel4pro/MainActivity.kt
+++ PADEL4PROAndroid/wear/src/main/java/feup/edu/lgp/padel4pro/MainActivity.kt
@@ -16,27 +16,12 @@
 package feup.edu.lgp.padel4pro
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.interaction.Interaction
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -84,7 +69,6 @@ fun WearApp(greetingName: String) {
         }
     )
 
-    val screenIndex = remember { mutableStateOf(screens[0]) }
     WearAppTheme {
         /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
          * version of LazyColumn for wear devices with some added features. For more information,
