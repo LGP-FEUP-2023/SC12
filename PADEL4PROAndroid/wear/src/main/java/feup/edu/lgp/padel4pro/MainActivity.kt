1947f86006904a1782a2901c37906a20b50c9608
up201904925 <up201904925@fe.up.pt>
2023-05-15 21:26:34 +0100

    horizontal swipe between screens - bug with scoreboard

--- PADEL4PROAndroid/wear/src/main/java/feup/edu/lgp/padel4pro/MainActivity.kt
+++ PADEL4PROAndroid/wear/src/main/java/feup/edu/lgp/padel4pro/MainActivity.kt
@@ -21,6 +21,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -60,32 +64,23 @@ data class Screen(val title: String, val content: @Composable () -> Unit)
 
 @Composable
 fun WearApp(greetingName: String) {
-    val screens = listOf(
-        Screen("Menu") {
-            Menu()
-        },
-        Screen("ScoreBoard") {
-            Scoreboard()
-        }
-    )
-
     WearAppTheme {
         /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
          * version of LazyColumn for wear devices with some added features. For more information,
          * see d.android.com/wear/compose.
          */
-        val scalingLazyListState: ScalingLazyListState = rememberScalingLazyListState()
+        val lazyListState: LazyListState = rememberLazyListState()
 
-        ScalingLazyColumn(
+        LazyRow(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-            state = scalingLazyListState,
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            state = lazyListState,
         ) {
-            items(1) {
-                Scoreboard()
-            }
             item {
                 Menu()
+            }
+            item {
+                Scoreboard()
             }
         }
     }
