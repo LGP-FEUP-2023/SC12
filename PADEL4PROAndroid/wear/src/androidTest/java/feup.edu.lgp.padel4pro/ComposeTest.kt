0e302e7e081ecd82ce5fb8bd5a1a6cd2de577639
caroucintra <up201906845@fe.up.pt>
2023-06-17 18:34:32 +0100

    fix: Testing started to run

--- PADEL4PROAndroid/wear/src/androidTest/java/feup.edu.lgp.padel4pro/ComposeTest.kt
+++ PADEL4PROAndroid/wear/src/androidTest/java/feup.edu.lgp.padel4pro/ComposeTest.kt
@@ -1,14 +1,21 @@
 package feup.edu.lgp.padel4pro
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 import feup.edu.lgp.padel4pro.MainActivity.*;
 
 class MyComposeTest {
 
-    // @get:Rule(order = 1)
-    // val composeRule = createAndroidComposeRule<MainActivity>()
-    // use createAndroidComposeRule<YourActivity>() if you need access to
+     //@get:Rule(order = 1)
+     //val composeRule = createAndroidComposeRule<MainActivity>()
+     //use createAndroidComposeRule<YourActivity>() if you need access to
     // an activity
 
     /*@Test
@@ -27,6 +34,7 @@ class MyComposeTest {
     }*/
 
     class TestButton {
+        /*
         @get:Rule
         val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
@@ -35,6 +43,18 @@ class MyComposeTest {
             val button = composeTestRule.onNode(hasTestTag("yourTestTag"), useUnmergedTree = true)
             button.assertIsDisplayed()
             button.performClick()
+        }
+         */
+
+        @get:Rule
+        val rule = createComposeRule()
+
+        @Test
+        fun testButton(){
+            rule.setContent { Padel4Pro(false) }
+            rule.onNodeWithText("Skip").performClick()
+
+            rule.onNodeWithContentDescription("highlights").assertExists()
         }
     }
 }
