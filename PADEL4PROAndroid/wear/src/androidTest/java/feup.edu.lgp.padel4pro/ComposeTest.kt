23da8185969574987d610442f3955b7c9034501e
caroucintra <up201906845@fe.up.pt>
2023-06-18 18:40:07 +0100

    Basic tests running

--- PADEL4PROAndroid/wear/src/androidTest/java/feup.edu.lgp.padel4pro/ComposeTest.kt
+++ PADEL4PROAndroid/wear/src/androidTest/java/feup.edu.lgp.padel4pro/ComposeTest.kt
@@ -1,64 +1,63 @@
 package feup.edu.lgp.padel4pro
 
+import androidx.activity.compose.setContent
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.test.GestureScope
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performGesture
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipe
+import androidx.compose.ui.test.swipeLeft
 import org.junit.Rule
 import org.junit.Test
 import feup.edu.lgp.padel4pro.MainActivity.*;
 
 class MyComposeTest {
 
-     //@get:Rule(order = 1)
-     //val composeRule = createAndroidComposeRule<MainActivity>()
-     //use createAndroidComposeRule<YourActivity>() if you need access to
-     //an activity
-
-    /*@Test
-    fun myTest() {
-        // Start the app
-        composeRule.setContent {
-            // MyAppTheme {
-            //     MainScreen(uiState = fakeUiState, /*...*/)
-            // }
-            Padel4Pro(false)
-        }
-
-        composeRule.onNodeWithText("Request Highlight").performClick() // (?)
-        composeRule.onNodeWithButton("").performClick() // (?)
-
-        composeRule.onNodeWithText("Welcome").assertIsDisplayed()
-    }*/
 
     @get:Rule
-    val rule = createAndroidComposeRule<MainActivity>()
-    // var rule = createComposeRule()
+    //val rule = createAndroidComposeRule<MainActivity>()
+    val rule = createComposeRule()
 
     @Test
-    fun testButton(){
+    fun testSkipButton(){
+        //rule.activity.setContent { Padel4Pro(false) }
         rule.setContent { Padel4Pro(false) }
         rule.onNodeWithText("Skip").performClick()
+
+        rule.onNodeWithContentDescription("highlights").assertIsDisplayed()
+        rule.onNodeWithContentDescription("videocam").assertIsDisplayed()
+        rule.onNodeWithContentDescription("see highlight").assertIsDisplayed()
     }
 
-    /* class TestButton {
-        /*
-        @get:Rule
-        val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
+    @Test
+    fun testMainMenu(){
+        //rule.activity.setContent { Padel4Pro(false) }
+        rule.setContent { Padel4Pro(false) }
+        rule.onNodeWithContentDescription("highlights").assertDoesNotExist()
 
-        @Test
-        fun testButtonClick() {
-            val button = composeTestRule.onNode(hasTestTag("yourTestTag"), useUnmergedTree = true)
-            button.assertIsDisplayed()
-            button.performClick()
-        }
-         */
+    }
 
+    @Test
+    fun testScoreMenu(){
+        //rule.activity.setContent { Padel4Pro(false) }
+        rule.setContent { Padel4Pro(false) }
 
-    }*/
+        rule.onNodeWithText("Skip").performClick()
+        rule.onRoot().performTouchInput { this.swipeLeft() }
+
+        rule.onNodeWithText("TEAM 1").assertIsDisplayed()
+        rule.onNodeWithText("TEAM 2").assertIsDisplayed()
+
+    }
+
 }
